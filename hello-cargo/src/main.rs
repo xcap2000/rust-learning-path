9d1d18bebe84b0480143dcd68a7b93ba95f32413
Carlos Adriano Portes <xcap2000@outlook.com>
2024-12-09 22:22:29 -0300

    Add new functions

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -7,6 +7,17 @@ fn main() {
     f5();
     f6();
     f7();
+    f8();
+    f9();
+    f10();
+
+    let formal = "Formal: Goodbye.";
+    let casual = "Casual: See you later!";
+    goodbye(formal);
+    goodbye(casual);
+
+    let num = 25;
+    println!("{} divided by 5 = {}", num, divide_by_5(num));
 }
 
 fn f1() {
@@ -81,4 +92,157 @@ fn f7() {
     println!("{}", uppercase_s);
     println!("{}", lowercase_f);
     println!("{}", smiley_face);
+}
+
+fn f8() {
+    // Tuple of length 3
+    let tuple_e = ('E', 5i32, true);
+    println!("{}{}{}", tuple_e.0, tuple_e.1, tuple_e.2);
+
+    // Declare a tuple of three elements
+    let tuple_e = ('E', 5i32, true);
+
+    // Use tuple indexing and show the values of the elements in the tuple
+    println!(
+        "Is '{}' the {}th letter of the alphabet? {}",
+        tuple_e.0, tuple_e.1, tuple_e.2
+    );
+}
+
+// Classic struct with named fields
+struct Student {
+    name: String,
+    level: u8,
+    remote: bool,
+}
+
+// Tuple struct with data types only
+struct Grades(char, char, char, char, f32);
+
+// Unit struct
+// struct Unit;
+
+fn f9() {
+    // Instantiate classic struct, specify fields in random order, or in specified order
+    let user_1 = Student {
+        name: String::from("Constance Sharma"),
+        remote: true,
+        level: 2,
+    };
+    let user_2 = Student {
+        name: String::from("Dyson Tan"),
+        // name: "Dyson Tan".to_string(),
+        level: 5,
+        remote: false,
+    };
+
+    // Instantiate tuple structs, pass values in same order as types defined
+    let mark_1 = Grades('A', 'A', 'B', 'A', 3.75);
+    let mark_2 = Grades('B', 'A', 'A', 'C', 3.25);
+
+    println!(
+        "{}, level {}. Remote: {}. Grades: {}, {}, {}, {}. Average: {}",
+        user_1.name, user_1.level, user_1.remote, mark_1.0, mark_1.1, mark_1.2, mark_1.3, mark_1.4
+    );
+    println!(
+        "{}, level {}. Remote: {}. Grades: {}, {}, {}, {}. Average: {}",
+        user_2.name, user_2.level, user_2.remote, mark_2.0, mark_2.1, mark_2.2, mark_2.3, mark_2.4
+    );
+}
+
+// enum WebEvent {
+//     // An enum variant can be like a unit struct without fields or data types
+//     WELoad,
+//     // An enum variant can be like a tuple struct with data types but no named fields
+//     WEKeys(String, char),
+//     // An enum variant can be like a classic struct with named fields and their data types
+//     WEClick { x: i64, y: i64 }
+// }
+
+// Define a tuple struct
+#[derive(Debug)]
+struct KeyPress(String, char);
+
+// Define a classic struct
+#[derive(Debug)]
+struct MouseClick {
+    x: i64,
+    y: i64,
+}
+
+// Define the WebEvent enum variants to use the data from the structs
+// and a boolean type for the page Load variant
+#[derive(Debug)]
+enum WebEvent {
+    WELoad(bool),
+    WEClick(MouseClick),
+    WEKeys(KeyPress),
+}
+
+fn f10() {
+    // Instantiate a MouseClick struct and bind the coordinate values
+    let click = MouseClick { x: 100, y: 250 };
+    println!("Mouse click location: {}, {}", click.x, click.y);
+
+    // Instantiate a KeyPress tuple and bind the key values
+    let keys = KeyPress(String::from("Ctrl+"), 'N');
+    println!("\nKeys pressed: {}{}", keys.0, keys.1);
+
+    // Instantiate WebEvent enum variants
+    // Set the boolean page Load value to true
+    let we_load = WebEvent::WELoad(true);
+    // Set the WEClick variant to use the data in the click struct
+    let we_click = WebEvent::WEClick(click);
+    // Set the WEKeys variant to use the data in the keys tuple
+    let we_key = WebEvent::WEKeys(keys);
+
+    // Print the values in the WebEvent enum variants
+    // Use the {:#?} syntax to display the enum structure and data in a readable form
+    println!(
+        "\nWebEvent enum structure: \n\n {:#?} \n\n {:#?} \n\n {:#?}",
+        we_load, we_click, we_key
+    );
+}
+
+fn goodbye(message: &str) {
+    println!("\n{}", message);
+}
+
+// fn divide_by_5(num: u32) -> u32 {
+//     num / 5 // return is optional on the last line, no semicolon should be added
+// }
+
+fn divide_by_5(num: u32) -> u32 {
+    if num == 0 {
+        // Return early
+        return 0;
+    }
+    num / 5
+}
+
+// Declare Car struct to describe vehicle with four named fields
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+}
+
+#[derive(PartialEq, Debug)]
+// Declare enum for Car transmission type
+enum Transmission {
+    // todo!("Fix enum definition so code compiles");
+    Manual,
+    SemiAuto,
+    Automatic,
+}
+
+// Build a "Car" by using values from the input arguments
+// - Color of car (String)
+// - Transmission type (enum value)
+// - Convertible (boolean, true if car is a convertible)
+fn car_factory(color: String, transmission: Transmission, convertible: bool) {
+    // Use the values of the input arguments
+    // All new cars always have zero mileage
+    let car: Car = todo!("Create an instance of a `Car` struct");
 }
